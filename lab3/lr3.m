79a8800ce7d148ca3ee1e048291f9b406a239297
VAnDm <klirens32@gmail.com>
2026-03-31 23:58:27 +0300

    otchetver1

--- lab3/lr3.m
+++ lab3/lr3.m
@@ -14,9 +14,9 @@ g = zeros(size(t));
 g(t >= t_1 & t <= t_2) = a; % Значения функции g(t)
 xi = 2*rand(size(t)) - 1; % Дискретные значения шума
 
-b = 1;
-c = 0.8;
-d = 50;
+b = 0.5;
+c = 0.5;
+d = 20;
 
 width = 800;
 height = 600;
@@ -24,81 +24,53 @@ resolution = 300;
 
 u = g + b*xi + c*sin(d*t);
 
-%figure;
-%plot(t, u, 'LineWidth', 1.5);
-%grid on;
-%xlim([-T/2-1 T/2+1]);
-%ylim([-1.5 5]);
-%xlabel("t", FontSize=24);
-%ylabel("u(t)", FontSize=24);
-%title("Зашумлённая g(t)", FontSize=24);
-%set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
-%exportgraphics(gcf, 'noise_g.png', 'Resolution', 300);
 
-
-%figure;
-%plot(t, g, 'LineWidth', 1.5);
-%grid on;
-%ylim([-1.5 5]);
-%xlim([-T/2-1 T/2+1]);
-%xlabel("t", FontSize=24);
-%ylabel("g(t)",FontSize=24);
-%title("Функция g(t)", FontSize=24);
-%set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
-%exportgraphics(gcf, 'function_g.png', 'Resolution', 300);
 
 
 U = fftshift(fft(u));
+G = fftshift(fft(g));
 
-%figure;
-%plot(v, abs(U), 'LineWidth', 1.5);
-%grid on;
-%ylim([-5 2000]);
-%xlim([-V/2-1 V/2+1]);
-%xlabel("v", FontSize=24);
-%ylabel("U(v)",FontSize=24);
-%title("Фурье-образ функции u(t)", FontSize=24);
-%set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
-%exportgraphics(gcf, 'im_noise.png', 'Resolution', 300);
 
-high_filter = zeros(size(v));
-high_filter(v <= -0.05 | v >= 0.05) = 1;
+%low_filter = zeros(size(v));
+%low_filter((v >= -8 & v <= -3.5) | (v >= -2.5 & v <= 2.5) | (v >= 3.5 & v <= 8)) = 1;
+%low_filter(v >= -1 & v <= 1) = 1;
+low_filter = ones(size(v));
+low_filter(v >= -1 & v <= 1) = 0;
 
-%figure;
-%plot(v, high_filter, 'LineWidth', 1.5);
-%grid on;
-%ylim([-0.5 2]);
-%xlim([-V/2-1 V/2+1]);
-%xlabel("v", FontSize=24);
-%ylabel("F(v)",FontSize=24);
-%title("Фильтр высоких частот F(t)", FontSize=24);
-%set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
-%exportgraphics(gcf, 'high_filter.png', 'Resolution', 300);
 
-hi_fil_u = U .* high_filter;
+hi_fil_u = U .* low_filter;
 
-%figure;
-%plot(v, hi_fil_u, 'LineWidth', 1.5);
-%grid on;
-%ylim([-75 150]);
-%xlim([-V/2-1 V/2+1]);
-%xlabel("v", FontSize=24);
-%ylabel("F(v)",FontSize=24);
-%title("Отфильтрованный Фурье-образ U(t)", FontSize=24);
-%set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
-%exportgraphics(gcf, 'high_filter_u.png', 'Resolution', 300);
+figure;
+plot(v, abs(U), 'LineWidth', 1.5, 'Color', 'cyan');
+hold on;
+plot(v, abs(hi_fil_u), 'LineWidth', 1.5, 'Color', 'red');
+hold on;
+plot(v, abs(G), '--g', 'LineWidth', 1.5, 'Color', 'black')
+grid on;
+ylim([-5 300]);
+xlim([-10 10]);
+xlabel("v", FontSize=24);
+ylabel("F_i(v)",FontSize=24);
+legend('F[u](v)', "F[u]_fil(v)", "F[g](v)");
+set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
+exportgraphics(gcf, 'furhigh1.png', 'Resolution', 300);
 
 filtered_g = ifft(ifftshift(hi_fil_u));
 
 figure;
-plot(v, filtered_g, 'LineWidth', 1.5);
+plot(t, u, 'LineWidth', 1.5, 'Color', 'cyan');
+hold on;
+plot(t, filtered_g, 'LineWidth', 1.5, 'Color', 'red');
+hold on;
+plot(t, g, 'LineWidth', 1.5, 'Color', 'black');
+hold on;
 grid on;
-ylim([-2.5 4]);
-xlim([-V/2-1 V/2+1]);
+ylim([-1.5 4]);
+xlim([-T/2-1 T/2+1]);
 xlabel("t", FontSize=24);
-ylabel("g'(t)",FontSize=24);
-title("Отфильтрованный сигнал g'(t)", FontSize=24);
+ylabel("g_i(t)",FontSize=24);
+legend('u(t)', "u_fil(t)", "g(t)");
 set(gcf, 'Units', 'pixels', 'Position', [100, 100, 1200, 800]);
-exportgraphics(gcf, 'high_filtered.png', 'Resolution', 300);
+exportgraphics(gcf, 'grhigh1.png', 'Resolution', 300);
 
 
